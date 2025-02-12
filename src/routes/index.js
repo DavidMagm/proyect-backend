eaa5010956b55b4cbab0cb098f4b9880ad12e520
david torres <david.martinezezguerra@gmail.com>
2025-02-12 09:36:18 -0500

    fix customers router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ const router = require('express').Router();
 const productsRouter = require('./products.route');
 const categoriesRouter = require('./categories.route');
 const usersRouter = require('./users.route');
-const custumersRouter = require('./customers.route');
+const customersRouter = require('./customers.route');
 
 function indexRoute(app) {
     app.use('/api/v1', router);
     router.use('/products', productsRouter);
     router.use('/categories', categoriesRouter);
     router.use('/users', usersRouter);
-    router.use('/custumers', custumersRouter);
+    router.use('/customers', customersRouter);
 }
 
 module.exports = indexRoute;
